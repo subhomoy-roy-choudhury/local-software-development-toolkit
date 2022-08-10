1ded2e145cf87fee27877f805b9ed3a2ab2d4108
Subhomoy <subhomoyrchoudhury@gmail.com>
2022-08-10 14:23:36 +0530

    added headers tag in wget operation

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN apt-get install -y wget curl unzip nano
 
 WORKDIR /home/fynd-data
 
-RUN wget -O setup_db.sh 'Authorization: token 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' https://raw.githubusercontent.com/subhomoy-roy-choudhury/fynd-local-db-setup/master/setup_db.sh
+RUN wget -O setup_db.sh --header 'Authorization: token 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' https://raw.githubusercontent.com/subhomoy-roy-choudhury/fynd-local-db-setup/master/setup_db.sh
 RUN chmod +x setup_db.sh
 
 RUN echo "Finished"
--- setup_db.sh
+++ setup_db.sh
@@ -8,7 +8,7 @@ echo "[2] silverbolt";
 read -p 'Enter the option :- ' option_key;
 
 function get_db() {
-    wget --no-check-certificate 'Authorization: token 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' https://raw.githubusercontent.com/subhomoy-roy-choudhury/fynd-local-db-setup/master/db_zip/${1}.zip -O ${1}.zip
+    wget --no-check-certificate --header 'Authorization: token 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' https://raw.githubusercontent.com/subhomoy-roy-choudhury/fynd-local-db-setup/master/db_zip/${1}.zip -O ${1}.zip
     unzip ${1}.zip
     mongorestore --db ${1} --gzip ${1}
 }
