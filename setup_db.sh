2a53d7cf5e99bdd2cf0bb1553920d7eda5ba2e1b
Subhomoy <subhomoyrchoudhury@gmail.com>
2022-08-10 13:40:47 +0530

    changes setup.sh logic

--- setup_db.sh
+++ setup_db.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
+db_names=("orbis" "silverbolt");
+
 echo "[1] orbis";
 echo "[2] silverbolt";
 
 read -p 'Enter the option :- ' option_key;
 
-if [[ $option_key == 1 ]];
-    then 
-        echo 'orbis';
-        echo mongo localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("orbis")';
-        wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1T8oqhWnmO3JtyYpXT3dftwt-F9vyC3Li' -O orbis.zip
-        unzip orbis.zip
-        mongorestore --db orbis --gzip orbis
-elif [[ $option_key == 2 ]];
-    then 
-        echo 'silverbolt';
-        echo mongo localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("silverbolt")';
-        wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1tRzIuHXDKRAEQdD0Kw5jmhLwQaWIcsoh' -O silverbolt.zip
-        unzip silverbolt.zip 
-        mongorestore --db silverbolt --gzip silverbolt
-fi
+function get_db() {
+    echo ${mongo localhost:27017 --eval 'db.getMongo().getDBNames().indexOf("${1}")'};
+    wget --no-check-certificate https://raw.githubusercontent.com/subhomoy-roy-choudhury/fynd-local-db-setup/master/db_zip/${1}.zip\?token\=GHSAT0AAAAAABXP2LPFFINS4OWZHHPZE36WYXTNW5Q -O ${1}.zip
+    unzip ${1}.zip
+    mongorestore --db ${1} --gzip ${1}
+}
+
+get_db ${db_names[$option_key - 1]};
