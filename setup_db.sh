70247bf20496af2e0b9a1fcaf95c3a49f0ba8142
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-08-28 14:23:30 +0530

    removed stdout and stderr

--- setup_db.sh
+++ setup_db.sh
@@ -18,7 +18,7 @@ for file in db_zip/*.zip
 
         db_names+=($base)
 
-        docker cp db_zip/${base}.zip $container_id:/home/fynd-data/db_zip/${base}.zip     #Syncing new files
+        docker cp db_zip/${base}.zip $container_id:/home/fynd-data/db_zip/${base}.zip &> /dev/null   #Syncing new files
         echo "[$file_count] ${base}"
     done
 
@@ -26,7 +26,7 @@ read -p 'Enter the option :- ' option_key;
 
 
 function get_db() {
-    docker exec -it $container_name bash -c "rm -rf ${1} && unzip -o db_zip/${1}.zip && mongorestore --db ${1} --gzip ${1}"
+    docker exec -it $container_name bash -c "rm -rf ${1} && unzip -o db_zip/${1}.zip && mongorestore --db ${1} --gzip ${1}" &> /dev/null
 }
 
 get_db ${db_names[$option_key - 1]};
