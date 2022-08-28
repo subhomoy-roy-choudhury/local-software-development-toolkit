b63c879ad94a24112fe418b316f45a32ae7c705c
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-08-28 13:43:30 +0530

    added dynamic mongo versioning based on local.env variables

--- install.sh
+++ install.sh
@@ -1,9 +1,37 @@
-echo "Creating database folder :-";
+DATABASE_DUMP_FOLDER_NAME="database";
+DATABASE_ZIP_FOLDER="db_zip";
+env_file_name="local.env";
+unamestr=$(uname)
 
-mkdir db_zip
-mkdir database
+echo "[+] Creating database folder";
 
-echo "Building and starting mongo:4.2.2 instance"
+{
+mkdir $DATABASE_ZIP_FOLDER
+mkdir $DATABASE_DUMP_FOLDER_NAME
+} &> /dev/null
+
+echo "[+] Checking for local.env file"
+
+FILE=local.env
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    echo "$FILE does not exist."
+fi
+
+echo "[+] Loading Environment variables"
+
+if [ "$unamestr" = 'Linux' ]; then
+
+  export $(grep -v '^#' $env_file_name | xargs -d '\n')
+
+elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+
+  export $(grep -v '^#' $env_file_name | xargs -0)
+
+fi
+
+echo "[+] Building and starting mongo:4.2.2 instance"
 docker-compose up --build -d
 
-echo "Finished"
+echo "[+] Finished"
