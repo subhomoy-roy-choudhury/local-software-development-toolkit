09b058a83a7117aad6863955a050788517caa14f
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-08-30 09:51:01 +0530

    Updated console outputs using color picker script

--- install.sh
+++ install.sh
@@ -1,25 +1,34 @@
+#!/bin/bash
+
 DATABASE_DUMP_FOLDER_NAME="database";
 DATABASE_ZIP_FOLDER="db_zip";
-env_file_name="local.env";
-unamestr=$(uname)
+ENV_FILE_NAME="local.env";
+UNAMESTR=$(uname)
 
 DEFAULT_MONGO_VERSION=4.2.2;
 DEFAULT_CONTAINER_NAME=local-mongo
 
-echo "[+] Creating database folder";
+#Finding Colors
+COLORS_FILE_PATH='./utils/find-colors.sh'
+
+COLOR_OFF=$(source $COLORS_FILE_PATH Color_Off);
+GREEN=$(source $COLORS_FILE_PATH Green);
+RED=$(source $COLORS_FILE_PATH Red);
+
+echo -e "${GREEN}[+] Creating database folder${COLOR_OFF}";      # printf is also used instead of echo -e
 
 {
 mkdir $DATABASE_ZIP_FOLDER
 mkdir $DATABASE_DUMP_FOLDER_NAME
 } &> /dev/null   # hide stderr and stdout output using /dev/null
 
-echo "[+] Checking for local.env file"
+echo -e "${GREEN}[+] Checking for local.env file${COLOR_OFF} "
 
 FILE=local.env
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo -e "${RED}[+] $FILE exists.${COLOR_OFF}"
 else 
-    echo "Creating $FILE"
+    echo -e "${GREEN}[+] Creating $FILE${COLOR_OFF}"
 
     read -p "Enter the MongoDB version [$DEFAULT_MONGO_VERSION]: " MONGO_VERSION;
     echo "MONGO_VERSION=${MONGO_VERSION:-$DEFAULT_MONGO_VERSION}" > $FILE
@@ -30,17 +39,17 @@ fi
 
 echo "[+] Loading Environment variables"
 
-if [ "$unamestr" = 'Linux' ]; then
+if [ "$UNAMESTR" = 'Linux' ]; then
 
-  export $(grep -v '^#' $env_file_name | xargs -d '\n')
+  export $(grep -v '^#' $ENV_FILE_NAME | xargs -d '\n')
 
-elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+elif [ "$UNAMESTR" = 'FreeBSD' ] || [ "$UNAMESTR" = 'Darwin' ]; then
 
-  export $(grep -v '^#' $env_file_name | xargs -0)
+  export $(grep -v '^#' $ENV_FILE_NAME | xargs -0)
 
 fi
 
-# echo "[+] Building and starting mongo:4.2.2 instance"
-# docker-compose up --build -d
+echo "[+] Building and starting mongo:4.2.2 instance"
+docker-compose up --build -d
 
-# echo "[+] Finished"
+echo "[+] Finished"
--- local_mongo/main.py
+++ local_mongo/main.py
@@ -1,2 +1,13 @@
+import docker
+
+class LocalMongo(object):
+    def __init__(self,client) -> None:
+        self.client=client
+        
+    def exec(self):
+        print(self.client.containers.list())
+
 def run():
     print("Local Mongo CLI")
+    client = docker.from_env()
+    local_mongo_obj = LocalMongo(client).exec()
