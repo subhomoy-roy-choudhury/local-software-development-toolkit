067a8b214af10bdad364406f767f247d10b2010e
Subhomoy Roy Choudhury <subhomoyrchoudhury@gmail.com>
2022-08-31 09:40:05 +0530

    added load-env fucntion to set env variables

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 DATABASE_DUMP_FOLDER_NAME="database";
 DATABASE_ZIP_FOLDER="db_zip";
@@ -15,20 +15,20 @@ COLOR_OFF=$(source $COLORS_FILE_PATH Color_Off);
 GREEN=$(source $COLORS_FILE_PATH Green);
 RED=$(source $COLORS_FILE_PATH Red);
 
-echo -e "${GREEN}[+] Creating database folder${COLOR_OFF}";      # printf is also used instead of echo -e
+echo "${GREEN}[+] Creating database folder${COLOR_OFF}";      # printf is also used instead of echo -e
 
 {
 mkdir $DATABASE_ZIP_FOLDER
 mkdir $DATABASE_DUMP_FOLDER_NAME
 } &> /dev/null   # hide stderr and stdout output using /dev/null
 
-echo -e "${GREEN}[+] Checking for local.env file${COLOR_OFF} "
+echo "${GREEN}[+] Checking for local.env file${COLOR_OFF} "
 
 FILE=local.env
 if [ -f "$FILE" ]; then
-    echo -e "${RED}[+] $FILE exists.${COLOR_OFF}"
+    echo "${RED}[+] $FILE exists.${COLOR_OFF}"
 else 
-    echo -e "${GREEN}[+] Creating $FILE${COLOR_OFF}"
+    echo "${GREEN}[+] Creating $FILE${COLOR_OFF}"
 
     read -p "Enter the MongoDB version [$DEFAULT_MONGO_VERSION]: " MONGO_VERSION;
     echo "MONGO_VERSION=${MONGO_VERSION:-$DEFAULT_MONGO_VERSION}" > $FILE
@@ -39,15 +39,7 @@ fi
 
 echo "[+] Loading Environment variables"
 
-if [ "$UNAMESTR" = 'Linux' ]; then
-
-  export $(grep -v '^#' $ENV_FILE_NAME | xargs -d '\n')
-
-elif [ "$UNAMESTR" = 'FreeBSD' ] || [ "$UNAMESTR" = 'Darwin' ]; then
-
-  export $(grep -v '^#' $ENV_FILE_NAME | xargs -0)
-
-fi
+source utils/load-env.sh local.env
 
 echo "[+] Building and starting mongo:4.2.2 instance"
 docker-compose up --build -d
